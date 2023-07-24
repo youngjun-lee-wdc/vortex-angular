a08118adb2cbb389ccb046e7a5b206f58aa2c257
Youngjun Lee <youngjun.lee1@wdc.com>
2023-07-24 15:58:36 -0700

    task update function implemented

--- src/app/lightbox/lightbox.component.ts
+++ src/app/lightbox/lightbox.component.ts
@@ -24,7 +24,6 @@ export class LightboxComponent{
     public dateSelected: {startDate: Date , endDate: Date};
     public uniqueTestPlans: any
 
-
     constructor(){
       this.minStartDate = new Date().toISOString()
     }
@@ -66,7 +65,24 @@ export class LightboxComponent{
           }, task.parent)
 
       }else{
-          gantt.updateTask(task.id);
+          // gantt.updateTask(task.id);
+
+          // check which variables got updated:
+
+
+
+          gantt.updateTask(this.taskId, {
+            id: this.taskId,
+            text: (this.testSuiteSelected === undefined) ? gantt.getTask(this.taskId).text : this.testSuiteSelected,
+            test_plan_dropdown: (this.testPlanSelected === undefined) ? gantt.getTask(this.taskId)['test_plan_dropdown'] : this.testPlanSelected,
+            FW_version: (this.firmwareVersionSelected === undefined) ? gantt.getTask(this.taskId)['FW_version'] : this.firmwareVersionSelected,
+            start_date: (this.dateSelected.startDate === undefined) ? gantt.getTask(this.taskId)['start_date'] : new Date(this.dateSelected.startDate), 
+            end_date: (this.dateSelected.endDate === undefined) ? gantt.getTask(this.taskId)['end_date'] : new Date(this.dateSelected.endDate),
+            duration: gantt.calculateDuration({
+              start_date: new Date(this.dateSelected.startDate),
+              end_date: new Date(this.dateSelected.endDate)
+            })
+          })
       }
    
       gantt.hideLightbox();
@@ -111,7 +127,6 @@ export class LightboxComponent{
           uniqueOptions.push(options[i])
         }
         this.uniqueTestPlans = uniqueOptions
-        console.log(uniqueOptions)
       })
     }
 
@@ -122,20 +137,11 @@ export class LightboxComponent{
         this.getUniqueTestPlans()
         this.initLightBox()
         const myModal = this.formModal
-
         myModal.show()
-
-
       }
 
       gantt.hideLightbox = () =>{
         this.formModal.hide()
       }
-      // gantt.hideLightbox = () =>{
-        // this.formModal.hide()
-      // }
-    }
-
-
-    
+    }    
 }
