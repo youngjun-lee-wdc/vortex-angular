ba52f459be45f3e265332708e0495006f1a24871
Youngjun Lee <youngjun.lee1@wdc.com>
2023-07-24 16:16:59 -0700

    clean up imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,36 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule  } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatDatepickerModule} from '@angular/material/datepicker'; 
-// import { MatNativeDateModule } from '@angular/material/'
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GanttComponent } from './gantt/gantt.component';
-import { MarkerDirective } from './marker.directive';
+import { HotkeyDirective } from './hotkey.directive';
 import { HotkeysDirective } from './hotkeys.directive';
+import { LightboxDataService } from './lightbox-data.service';
+import { LightboxComponent } from './lightbox/lightbox.component';
+import { MarkerDirective } from './marker.directive';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { TooltipDirective } from './tooltip.directive';
 import { ZoomDirective } from './zoom.directive';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
-import { AccordionModule } from 'ngx-bootstrap/accordion'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HotkeyDirective } from './hotkey.directive';
-import { LightboxComponent } from './lightbox/lightbox.component';
-import { LightboxDirective } from './lightbox.directive';
-import { LightboxDataService } from './lightbox-data.service';
-import { MatNativeDateModule } from '@angular/material/core';
-import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 
 @NgModule({
   declarations: [
     AppComponent,
     GanttComponent,
+    LightboxComponent,
+    ToolbarComponent,
+
     MarkerDirective,
     HotkeysDirective,
     TooltipDirective,
     ZoomDirective,
-    ToolbarComponent,
     HotkeyDirective,
-    LightboxComponent,
-    LightboxDirective,
+    // LightboxDirective,
     
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -43,11 +39,8 @@ import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
     AppRoutingModule,
     BrowserAnimationsModule,
     TypeaheadModule.forRoot(),
-    AccordionModule.forRoot(),
     NgxDaterangepickerMd.forRoot(),
     FormsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     ReactiveFormsModule
   ],
   providers: [LightboxDataService],
